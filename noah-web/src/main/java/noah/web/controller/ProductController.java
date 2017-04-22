140fd7d70f667d275836c3ee96d63dadc01c3c95
taoyu0608 <taoyu0608@gmail.com>
2017-04-22 16:38:33 +0800

    1. 新增圖片的顯示功能 2. 細拆 cookTypeView 3. 整合前端選取單位並即時計算金額的功能

--- noah-web/src/main/java/noah/web/controller/ProductController.java
+++ noah-web/src/main/java/noah/web/controller/ProductController.java
@@ -17,6 +17,7 @@ import noah.core.model.Product;
 import noah.core.service.ProductService;
 import noah.web.converter.ProductConverter;
 import noah.web.converter.ProductViewConverter;
+import noah.web.view.CookTypeView;
 import noah.web.view.ProductView;
 
 @Controller
@@ -47,7 +48,7 @@ public class ProductController {
 //		return productViews;
 		
 		Double count = (Double) (Math.random() * 5);
-		List results = mockProduct(category, count.intValue() +1);
+		List results = mockProduct(category, count.intValue() +2);
 		System.err.println(results);
 		return results;
 	}
@@ -65,14 +66,14 @@ public class ProductController {
 	private List<ProductView> mockProduct(Integer category, Integer count) {
 		List<ProductView> mockViews = Lists.newArrayList();
 		
-		for ( int i=0 ; i < count ; i++ ) {
+		for ( Integer i=0 ; i < count ; i++ ) {
 			ProductView view = new ProductView();
 			view.setId((long) (i+1));
 			view.setCategory(category);
 			if ( i%3 == 0 ) {
-				view.setCookTypes(Lists.newArrayList(1, 2));
+				view.setCookTypes(mockCookType(false));
 			} else {
-				view.setCookTypes(Lists.newArrayList(1, 2, 3));
+				view.setCookTypes(mockCookType(true));
 			}
 			view.setDisplayName("咖啡品牌＿" + i);
 			view.setName("品牌_" + i);
@@ -81,10 +82,38 @@ public class ProductController {
 			} else {
 				view.setUnitPrice(BigDecimal.valueOf(250));
 			}
+			Integer imgIndex = (i+1);
+			view.setImageName(imgIndex.toString());
+			view.setImageDisplayName(i + "咖啡");
 			
 			mockViews.add(view);
 		}
 		
+		System.err.println(mockViews);
 		return mockViews;
 	}
+	
+	private List<CookTypeView> mockCookType(boolean mockFull) {
+		List<CookTypeView> cookTypeViews = Lists.newArrayList();
+		
+		CookTypeView cookTypeView = new CookTypeView();
+		cookTypeView.setCookTypeId(1);
+		cookTypeView.setCookTypeName("淺焙");
+		cookTypeViews.add(cookTypeView);
+		
+		CookTypeView cookTypeView2 = new CookTypeView();
+		cookTypeView2.setCookTypeId(2);
+		cookTypeView2.setCookTypeName("中焙");
+		cookTypeViews.add(cookTypeView2);
+		
+		if ( mockFull ) {
+			CookTypeView cookTypeView3 = new CookTypeView();
+			cookTypeView3.setCookTypeId(3);
+			cookTypeView3.setCookTypeName("深焙");
+			cookTypeViews.add(cookTypeView3);
+		}
+		
+		return cookTypeViews;
+	}
+	
 }
